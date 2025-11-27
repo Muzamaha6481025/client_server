6c66f87d884c83ae364f62ca5a80de33d4d78fc9
Muzamaha6481025 <zakymaulanaharyanto@gmail.com>
2025-11-27 09:35:02 +0700

    Add files via upload

---
+++ CWindowsSystem32cmd.exe - python  client1.py
@@ -0,0 +1,10 @@
+Microsoft Windows [Version 10.0.26100.6899]
+(c) Microsoft Corporation. All rights reserved.
+
+C:\Users\ASUS\Downloads\cmd>python client.py
+Host: localhost
+Port: 001
+hai bro, apa kabar?
+hai juga bro, kabar ku baik
+yo kabar
+mantap
